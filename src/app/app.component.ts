7ed16832d3b959d790f91f87792572152ab1fddd
Syed Aman <sna274@nyu.edu>
2016-09-23 20:43:06 -0400

    create an array of heroes to later display and fetch from a web service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,19 @@ export class Hero{
   name : string;
 }
 
+const ListOfHeroes: Hero[] = [
+  { id: 1, name: 'Mr. Nice'},
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
