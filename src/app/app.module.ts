b2f1b07e9fd17298d533295c4dbc1e8e17b7aa6d
Syed Aman <sna274@nyu.edu>
2016-09-28 20:04:18 -0400

    import HeroDetailsComponent to root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HeroDetailComponent
   ],
   imports: [
     BrowserModule,
