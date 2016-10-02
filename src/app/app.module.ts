6272d50332eebacce1c0c22b930c3d0eb87ee8ca
Syed Aman <sna274@nyu.edu>
2016-10-02 07:22:35 -0400

    generate heroes component blueprint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent, HeroesComponent
   ],
   imports: [
     BrowserModule,
