aab2aa295b965112296ec43ed0d10e76377a288a
Syed Aman <sna274@nyu.edu>
2016-10-02 08:25:38 -0400

    move AppComponent and update URLs

--- src/app/hero.spec.ts
+++ src/app/hero.spec.ts
@@ -2,7 +2,7 @@
 /* tslint:disable:no-unused-variable */
 
 import { addProviders, async, inject } from '@angular/core/testing';
-import {Hero} from './hero';
+import { Hero } from './hero';
 
 describe('Hero', () => {
   it('should create an instance', () => {
