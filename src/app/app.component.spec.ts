07ac4f96b118d1eb0244e92d1d97afc565544c4a
Syed Aman <sna274@nyu.edu>
2016-10-02 08:20:52 -0400

    generate new AppComponent blueprint

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('Component: App', () => {
+  it('should create an instance', () => {
+    let component = new AppComponent();
+    expect(component).toBeTruthy();
+  });
+});
