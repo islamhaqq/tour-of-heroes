88bdb3d0279876a392bf419a8c6c14c9ab62a76d
Syed Aman <sna274@nyu.edu>
2016-10-07 16:03:48 -0400

    delete seperate routing file. Move to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroService } from './hero.service';
-import { routing } from './app.routing';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
+  ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    routing
-  ],
   providers: [
     HeroService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 
-export class AppModule { }
+export class AppModule { 
+}
