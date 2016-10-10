9789b41aa0db8f3ca56dfac69a58a69cde0667ed
Syed Aman <sna274@nyu.edu>
2016-10-10 18:04:20 -0400

    rename DashboardComponent selector

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'dashboard-view',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
