cbeb2b99796b8808c3929f979630cf89f137380a
Syed Aman <sna274@nyu.edu>
2016-10-11 11:58:56 -0400

    import HeroService & Hero to use array of Heroes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
 @Component({
   selector: 'dashboard-view',
   templateUrl: './dashboard.component.html',
