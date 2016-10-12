d50fb4d66d3200dc05da5fcdcee6a387547da724
Syed Aman <sna274@nyu.edu>
2016-10-12 19:34:32 -0400

    create a route for HeroDetailComponent's view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
       {
         path: 'dashboard',
         component: DashboardComponent
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
       }
     ])
   ],
