156efe1697807bf70fe091e5c979fa6b67b73a32
Syed Aman <sna274@nyu.edu>
2016-10-14 16:27:50 -0400

    import ActivatedRoute and Params for id parameter

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
 
 import { Hero } from '../Hero';
 import { HeroService } from '../hero.service';
