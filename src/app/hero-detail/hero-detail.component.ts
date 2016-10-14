cf1d41686fccbcc417bacd3985da43c9640b795a
Syed Aman <sna274@nyu.edu>
2016-10-14 16:28:37 -0400

    import Location for backwards app navigation

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { Hero } from '../Hero';
 import { HeroService } from '../hero.service';
