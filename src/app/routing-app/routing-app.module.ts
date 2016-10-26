175718e921459e89d8919d5c929069220637e8c1
Syed Aman <sna274@nyu.edu>
2016-10-26 08:32:53 -0400

    moved routes into a routes variabel

--- src/app/routing-app/routing-app.module.ts
+++ src/app/routing-app/routing-app.module.ts
@@ -1,35 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoutingAppComponent } from './routing-app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { HeroesComponent } from '../heroes/heroes.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'heroes',
+    component: HeroesComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'detail/:hero_id',
+    component: HeroDetailComponent
+  }
+];
+
 @NgModule({
   imports: [
     CommonModule,
     // Define routes
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent,
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'detail/:hero_id',
-        component: HeroDetailComponent
-      }
-    ])
+    RouterModule.forRoot(routes)
   ],
   declarations: [RoutingAppComponent]
 })
