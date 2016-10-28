6b79531cbc78e110ea94a9de051a45de02963bac
Syed Aman <sna274@nyu.edu>
2016-10-28 12:33:41 -0400

    remove RouterModule import from AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
